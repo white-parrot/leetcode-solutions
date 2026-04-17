9e80d3a726fff5834e2db939965927e5c1dd321a
shreyansh <82340454+white-parrot@users.noreply.github.com>
2026-04-17 23:58:14 +0530

    Update 014. Longest Common Prefix.java

--- 014. Longest Common Prefix.java
+++ 014. Longest Common Prefix.java
@@ -1,1 +1,21 @@
-
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        String outcome = strs[0];
+        for(String s : strs){
+            int k = 0;
+            while(k < outcome.length()){
+                if(k == s.length()){
+                    outcome = s; break;
+                }
+                if(outcome.charAt(k) == s.charAt(k)){
+                    k++;
+                    continue;
+                }else{
+                    outcome = (k == 0 ? "" : outcome.substring(0, k));
+                    break;
+                }
+            }
+        }
+        return outcome;
+    }
+}
